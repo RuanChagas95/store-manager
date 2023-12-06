a336bb595ce4808d7768560aa6e96c3406036114
Ruan Chagas <ruanchagas95@gmail.com>
2023-12-06 01:37:30 -0300

    chore: req4 validate product

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,10 +1,11 @@
 const route = require('express').Router();
+const { verifyProduct } = require('../middlewares/products');
 
 const { getAllController, getByidController,
   createProductController } = require('../controllers/products');
 
 route.get('/:id', getByidController);
 route.get('/', getAllController);
-route.post('/', createProductController);
+route.post('/', verifyProduct, createProductController);
 
 module.exports = route;
